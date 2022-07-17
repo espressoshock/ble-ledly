27f9a1ff64f044efd144bda9b4506911b85c654d
Vincenzo Buono <vincenzo.buono.phd@gmail.com>
2022-07-17 18:33:27 +0200

    feat(device): async-traits added

--- src/device/traits.rs
+++ src/device/traits.rs
@@ -2,22 +2,26 @@ use std::error::Error;
 use btleplug::api::Characteristic;
 use btleplug::platform::Peripheral;
 
+use async_trait::async_trait;
+
+#[async_trait]
 pub trait Device {
     fn new (alias: &str, peripheral: Peripheral, write_chars: Option<Vec<Characteristic>>, read_chars: Option<Vec<Characteristic>>) -> Self;
     fn alias(&self) -> &str;
     fn peripheral(&self) -> &Option<Peripheral>;
     fn write_char(&self, nth: Option<usize>) -> Option<&Characteristic>;
-    fn write_raw(&self, raw_bytes: &Vec<u8>);
+    async fn write_raw(&self, raw_bytes: &Vec<u8>);
 
     fn set_peripheral(&mut self, peripheral: Peripheral);
     fn add_write_characteristic(&mut self, characteristic: Characteristic);
     fn add_read_characteristic(&mut self, characteristic: Characteristic);
 }
 
+#[async_trait]
 pub trait Light {
-    fn turn_on(&self);
-    fn turn_off(&self);
-    fn set_brightness(&self, value: u8) -> Result<(), Box<dyn Error>>;
+    async fn turn_on(&self);
+    async fn turn_off(&self);
+    async fn set_brightness(&self, value: u8) -> Result<(), Box<dyn Error>>;
 }
 
 pub trait RGB : Light {
