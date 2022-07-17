48dab5ee6739d0aee09704caf184006e0fbb82f9
Vincenzo Buono <vincenzo.buono.phd@gmail.com>
2022-07-17 08:13:15 +0200

    feat(device/traits): Device trait added

--- src/device/traits.rs
+++ src/device/traits.rs
@@ -2,6 +2,17 @@ use std::error::Error;
 use btleplug::api::Characteristic;
 use btleplug::platform::Peripheral;
 
+pub trait Device {
+    fn new (alias: &str, peripheral: Peripheral, write_chars: Option<Vec<Characteristic>>, read_chars: Option<Vec<Characteristic>>) -> Self;
+    fn alias(&self) -> &str;
+    fn peripheral(&self) -> &Option<Peripheral>;
+    fn write_char(&self, nth: Option<usize>) -> Option<&Characteristic>;
+    fn write_raw(&self, raw_bytes: &Vec<u8>);
+
+    fn set_peripheral(&mut self, peripheral: Peripheral);
+    fn add_write_characteristic(&mut self, characteristic: Characteristic);
+    fn add_read_characteristic(&mut self, characteristic: Characteristic);
+}
 
 pub trait Light {
     fn turn_on(&self);
