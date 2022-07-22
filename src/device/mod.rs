b493b47a0c0e857b4f0506e0c10d191b329d3608
Vincenzo Buono <vincenzo.buono.phd@gmail.com>
2022-07-22 09:13:30 +0200

    feat(device): device, disconnect, connect trait added
    opt lifetime added

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,2 +1,25 @@
-pub mod traits;
+use crate::error::BluetoothError;
+
+use btleplug::api::Characteristic;
+use btleplug::api::{Peripheral as _, WriteType};
+use btleplug::platform::Peripheral;
+
+use async_trait::async_trait;
+
 pub mod led_device;
+
+pub trait Device<'p> {
+    fn peripheral(&self) -> &Option<Peripheral>;
+    fn write_char(&self) -> Option<&'p Characteristic>;
+}
+
+#[async_trait]
+pub trait Disconnect {
+    async fn leave(&self) -> Result<(), BluetoothError>;
+}
+pub trait Connect {}
+#[async_trait]
+pub trait Write {
+    async fn push(&self, raw_bytes: &[u8]) -> Result<(), BluetoothError>;
+}
+
