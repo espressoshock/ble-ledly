279ac82c9386bfc42d4ccd758a8ab8e351c1fea1
Vincenzo Buono <vincenzo.buono.phd@gmail.com>
2022-07-26 15:42:47 +0200

    feat(led_device): default_write characteristic removed

--- src/device/led_device.rs
+++ src/device/led_device.rs
@@ -1,29 +1,11 @@
-use btleplug::api::{CharPropFlags, Characteristic, Peripheral as _};
+use btleplug::api::{Characteristic, Peripheral as _};
 use btleplug::platform::Peripheral;
 
-use btleplug::api::bleuuid::uuid_from_u16;
-use enumflags2::_internal::RawBitFlags;
 use uuid::Uuid;
 
 use std::fmt;
 
 use crate::device::Device;
-use enumflags2::{bitflags, BitFlags};
-
-// wrapper for native ble charprops
-#[bitflags]
-#[repr(u8)]
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub enum CharactericKind {
-    Broadcast = 0x01,
-    Read = 0x02,
-    WriteWithoutResponse = 0x04,
-    Write = 0x08,
-    Notify = 0x10,
-    Indicate = 0x20,
-    AuthenticatedSignedWrites = 0x40,
-    ExtendedProperties = 0x80,
-}
 
 #[derive(Debug)]
 pub struct LedDevice {
@@ -38,12 +20,7 @@ pub struct LedDevice {
     // default communication chars
     write_char: Option<Characteristic>,
     read_char: Option<Characteristic>,
-
-    // default write char uuid
-    write_char_uuid: Uuid,
 }
-// common to generic ble ic(s)
-pub const DEFAULT_WRITE_CHARACTERISTIC_UUID: Uuid = uuid_from_u16(0xFFD9);
 
 impl Device for LedDevice {
     fn new(
@@ -52,7 +29,6 @@ impl Device for LedDevice {
         peripheral: Option<Peripheral>,
         write_char: Option<Characteristic>,
         read_char: Option<Characteristic>,
-        write_char_uuid: Option<Uuid>,
     ) -> Self {
         Self {
             name: name.to_string(),
@@ -60,7 +36,6 @@ impl Device for LedDevice {
             peripheral,
             write_char: write_char.clone(),
             read_char: read_char.clone(),
-            write_char_uuid: write_char_uuid.unwrap_or(DEFAULT_WRITE_CHARACTERISTIC_UUID),
         }
     }
     // TODO: remove getters and setters
@@ -82,9 +57,6 @@ impl Device for LedDevice {
     fn peripheral(&self) -> Option<&Peripheral> {
         self.peripheral.as_ref()
     }
-    fn write_char_uuid(&self) -> &Uuid {
-        &self.write_char_uuid
-    }
     fn write_char(&self) -> Option<&Characteristic> {
         self.write_char.as_ref()
     }
@@ -92,7 +64,9 @@ impl Device for LedDevice {
         self.read_char.as_ref()
     }
     fn default_write_characteristic_uuid(&self) -> Uuid {
-        DEFAULT_WRITE_CHARACTERISTIC_UUID.clone()
+        // DEFAULT_WRITE_CHARACTERISTIC_UUID.clone()
+        let my_uuid = Uuid::new_v4();
+        my_uuid
     }
 
     //--------//
@@ -106,9 +80,6 @@ impl Device for LedDevice {
     }
     fn set_peripheral(&mut self, peripheral: Peripheral) {
         self.peripheral = Some(peripheral);
-    }
-    fn set_write_char_uuid(&mut self, char_uuid: Uuid) {
-        self.write_char_uuid = char_uuid;
     }
     fn set_write_char(&mut self, characteristic: &Characteristic) {
         self.write_char = Some(characteristic.clone());
