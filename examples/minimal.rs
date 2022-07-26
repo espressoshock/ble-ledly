ba38489a3c45309c7795252da65e7f460cebc581
Vincenzo Buono <vincenzo.buono.phd@gmail.com>
2022-07-26 18:15:28 +0200

    feat(example): with minimal conf. added

---
+++ examples/minimal.rs
@@ -0,0 +1,31 @@
+use ble_ledly::capability::color::*;
+use ble_ledly::communication_protocol::generic_rgb::GenericRGB;
+use ble_ledly::controller::Controller;
+use ble_ledly::device::led_device::LedDevice;
+use ble_ledly::device::{ CharKind, UuidKind};
+
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Create a new Light controller with prefix
+    // Auto-filter devices that contain the prefix
+    let mut controller = Controller::<LedDevice>::new_with_prefix("QHM-").await?;
+
+    // Connect
+    controller.connect().await?;
+
+    // Choose your communication protocol
+    let protocol = GenericRGB::default();
+
+    // set default write characteristic for all connected 
+    // devices
+    controller.set_all_char(&CharKind::Write, &UuidKind::Uuid16(0xFFD9))?;
+
+    // Setting first found light color to red
+    let first_light = controller.list().get(0).unwrap();
+    first_light.color(&protocol, 255, 0, 0).await?;
+
+
+    Ok(())
+}
