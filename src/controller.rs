ee6693bd7da264e0296872bd0a0daed1ea8c323b
Vincenzo Buono <vincenzo.buono.phd@gmail.com>
2022-07-27 06:37:57 +0200

    doc(controller): rustdoc comments added

--- src/controller.rs
+++ src/controller.rs
@@ -16,7 +16,14 @@ pub struct Controller<D: Device> {
 }
 
 impl<D: Device> Controller<D> {
-    // Constructor //
+    /// Creates a new `Device` controller
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut controller = Controller::<LedDevice>::new().await?;
+    ///
+    /// ```
     pub async fn new() -> Result<Controller<D>, BluetoothError> {
         let ble_manager = Manager::new().await?;
 
@@ -33,6 +40,18 @@ impl<D: Device> Controller<D> {
             led_devices: Vec::<D>::new(),
         })
     }
+
+    /// Creates a new `Device` controller with `Prefix`.
+    /// The `prefix` is used to automatically filter the
+    /// devices found during `device_discovery()`; The filter looks
+    /// if the `prefix` __id contained__ in the device name.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut controller = Controller::<LedDevice>::new_with_prefix("QHM-").await?;
+    ///
+    /// ```
     pub async fn new_with_prefix(prefix: &str) -> Result<Controller<D>, BluetoothError> {
         let ble_manager = Manager::new().await?;
 
@@ -49,6 +68,17 @@ impl<D: Device> Controller<D> {
             led_devices: Vec::<D>::new(),
         })
     }
+
+    /// Sets all the devices default _Characteristic_ (Write or Read)
+    /// to the provided value. Global shortcut, instead of setting, per-device _Characteristic_
+    /// configuration
+    ///
+    /// # Examples
+    ///
+    /// ```
+    // controller.set_all_char(&CharKind::Write, &UuidKind::Uuid16(0xFFD9))?;
+    ///
+    /// ```
     pub fn set_all_char(
         &mut self,
         char_kind: &CharKind,
@@ -67,12 +97,32 @@ impl<D: Device> Controller<D> {
     pub fn ble_manager(&self) -> &Manager {
         &self.ble_manager
     }
+
+    /// Return a list (Vec<D>) of all the connected devices.
+    /// The list is empty until devices are connected to the controller.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let connected_lights = controller.list();
+    ///
+    /// ```
     pub fn list(&mut self) -> &mut Vec<D> {
         &mut self.led_devices
     }
+
     //------------------//
     // Device Discovery //
     //------------------//
+    /// Discover _ble devices_ by running a scan op. on the default adapter
+    /// and returns the found _devices__.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let led_devices = controller.device_discovery().await?;
+    ///
+    /// ```
     pub async fn device_discovery(&self) -> Result<Vec<D>, BluetoothError> {
         self.ble_adapter.start_scan(ScanFilter::default()).await?;
         time::sleep(Duration::from_secs(2)).await;
@@ -96,12 +146,38 @@ impl<D: Device> Controller<D> {
     //---------//
     // Connect //
     //---------//
+    /// Standalone `connect()` that can be used in conjunction with
+    /// `Controller::new_with_prefix(prfix)`; it automatically runs
+    /// a `device_discovery()` and connects to all devices that match `prefix`.
+    /// If no `prefix` is provided it attemps to connect to all available devices.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// controller.connect().await?;
+    ///
+    /// ```
     pub async fn connect(&mut self) -> Result<(), BluetoothError> {
         // Discover devices //
         let led_devices = self.device_discovery().await?;
         self._connect(led_devices).await?;
         Ok(())
     }
+
+    /// Connect to the devices passed as function's argument.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let led_devices = controller.device_discovery().await?;
+    /// filter devices
+    /// let lights: Vec<LedDevice> = led_devices
+    ///    .into_iter()
+    ///   .filter(|device| device.name.contains("QHM-"))
+    ///    .collect();
+    /// controller.connect_with_devices(lights).await?;
+    ///
+    /// ```
     pub async fn connect_with_devices(
         &mut self,
         led_devices: Vec<D>,
